8be1beb65a859bf3269edcd0d1230170ee769a3e
dasartini <adriansartinid@gmail.com>
2024-09-10 22:03:32 +0100

    modified form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import FormStyle from '../styles/FormStyle';
-import gardening3 from '../src/assets/gardening3.jpg'
+import gardening3 from '../src/assets/gardening3.jpg';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     email: '',
     message: '',
+    sender: '',
+    phoneNumber: '',
   });
+  const [attachment, setAttachment] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setAttachment(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,17 +28,26 @@ const ContactForm = () => {
     });
   };
 
+  const handleFileChange = (e) => {
+    setAttachment(e.target.files[0]);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    
+    const data = new FormData();
+    data.append('email', formData.email);
+    data.append('message', formData.message);
+    data.append('sender', formData.sender);
+    data.append('phoneNumber', formData.phoneNumber);
+    if (attachment) {
+      data.append('attachment', attachment);
+    }
+
     try {
       const response = await fetch('https://email-form-backend.onrender.com/api/send-email', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+        body: data,
       });
 
       if (response.ok) {
@@ -40,41 +61,71 @@ const ContactForm = () => {
   };
 
   return (
- <FormStyle>
-  <div class="form-container">
-   
-  <div class="form-left">
-  <h1>Send us a enquiry</h1>
-    <form onSubmit={handleSubmit}>
-      <div class="form-group">
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
+    <FormStyle>
+      <div className="form-container">
+        <div className="form-left">
+          <h1>Send us an enquiry</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Your Name:</label>
+              <input
+                name="sender"
+                value={formData.sender}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Phone number:</label>
+              <input
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Message:</label>
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group attachment-group">
+              <label>Attachment:</label>
+              <input
+                type="file"
+                onChange={handleFileChange}
+                ref={fileInputRef}
+              />
+              {attachment && (
+                <button onClick={handleClick} className="cancel-btn">
+                  Remove Attachment
+                </button>
+              )}
+            </div>
+            <button type="submit" className="submit-btn">Send</button>
+          </form>
+        </div>
+        <div className="form-right">
+          <img src={gardening3} alt="Contact Us Image" />
+        </div>
       </div>
-      <div class="form-group">
-        <label>Message:</label>
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit" class="submit-btn">Send</button>
-    </form>
-  </div>
-  <div class="form-right">
-    <img src={gardening3} alt="Contact Us Image" />
-  </div>
-</div>
-
     </FormStyle>
   );
 };
 
 export default ContactForm;
+
